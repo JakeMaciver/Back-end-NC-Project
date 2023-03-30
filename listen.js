204bf5b7b0585cbd15eba242ffa11abe5363fb08
JakeMaciver <Jacobmaciverwork@gmail.com>
2023-03-30 11:12:44 +0100

    add error handling to listening for port function

--- listen.js
+++ listen.js
@@ -2,7 +2,7 @@ const  app  = require('./app');
 
 const {PORT = 9091 } = process.env;
 
-app.listen(PORT, () => {
+app.listen(PORT, (err) => {
 	if (err) console.log(`err: ${err}`);
 	else console.log(`listening on port ${PORT}`);
 });
